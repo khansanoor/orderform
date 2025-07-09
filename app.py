91e546a63729dd826edaeccf6c4cd13c400db278
khansanoor <82741826+khansanoor@users.noreply.github.com>
2025-07-09 17:55:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from twilio.rest import Client
 
-st.title("☕ Café Order Form")
+st.title("Drinks Menu")
 st.markdown("Select your drink and enter your contact info.")
