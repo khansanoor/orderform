391d39042ed620f691bdbd70d24fe636f4ba0bbf
khansanoor <82741826+khansanoor@users.noreply.github.com>
2025-07-09 17:38:20 -0500

    Update and rename code.py to app.py

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from twilio.rest import Client
+
+st.title("☕ Café Order Form")
+st.markdown("Select your drink and enter your contact info.")
